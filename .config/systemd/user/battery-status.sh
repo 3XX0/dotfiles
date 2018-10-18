7c3b1ef662a6188c49034d301b91ce9f8ee7857c
Jonathan Calmels <jcalmels@nvidia.com>
2018-10-18 00:04:57 -0700

    Remove laptop specific config

--- .config/systemd/user/battery-status.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
-STATUS=$(cat /sys/class/power_supply/BAT0/status)
-
-if [ "$CAPACITY" -lt 10 ] && [ "$STATUS" = "Discharging" ]; then
-    [ "$CAPACITY" -lt 5 ] && URGENCY="error" || URGENCY="warning"
-    i3-nagbar -t $URGENCY -m "WARNING, low battery: $CAPACITY%"
-fi
